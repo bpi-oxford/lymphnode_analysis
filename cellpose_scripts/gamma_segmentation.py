753ae2734db470d43ccbc4ec2ab23977f51fb114
Ed Wheeler <edward.wheeler@kellogg.ox.ac.uk>
2025-06-04 09:40:01 +0100

    moved some redundantscripts to scripts_to_remove

--- cellpose_scripts/gamma_segmentation.py
+++ cellpose_scripts/gamma_segmentation.py
@@ -7,9 +7,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from cellpose_scripts.cellpose_batch import process_video_with_multiprocessing
 
+'''
+A simple function to apply gamma transformation to an image. The gamma transformation is called 
+in the main_class.py script - cellpose_segmentation function. 
+'''
+
 def gamma_transform(image, gamma):
     """
     Apply gamma transformation to the image.
+
+    INPUT:
+    ----------------------------------------------------
+    image: np.ndarray. The input image to be transformed.
+    gamma: float. The gamma value for the transformation.
+
+    OUTPUT:
+    ----------------------------------------------------
+    transformed_image: np.ndarray. The gamma-transformed image.
+
     """
     # Normalize the image to [0, 1]
     image_normalized = (image - np.min(image)) / (np.max(image) - np.min(image))
@@ -21,27 +36,4 @@ def gamma_transform(image, gamma):
     transformed_image = transformed_image * (np.max(image) - np.min(image)) + np.min(image)
     
     return transformed_image
-
-
-if __name__ == "__main__":
-    # Load the image
-    image_path = r"/home/edwheeler/Documents/cropped_region_2_motile/b2-2a_2c_pos6-01_deskew_cgt_cropped_for_segmentation_motile_region.tif"
-    image = tiff.imread(image_path)
-    custom_model_path = r"/home/edwheeler/Documents/training_data/train/models/CP_20250430_181517"
-
-    num_t_steps = image.shape[0]
-    # Define gamma values
-    gamma_values = [1.25, 1.5, 1.75]
-
-    for gamma in gamma_values:
-        gamma_image = gamma_transform(image, gamma)
-        gamma_path = image_path[0:-4] + 'gamma' + str(gamma) + '.tif'
-
-        gamma_masks = process_video_with_multiprocessing(gamma_image , gamma_path, custom_model_path)
-        print(gamma_masks[0].shape)
-        print(gamma_masks)
-
-        save_gamma_path = gamma_path[0:-4] + '_seg_masks_stack.tif'
-        tiff.imwrite(save_gamma_path , gamma_masks.astype(np.uint16))
-        print(f"Gamma transformed masks saved to {save_gamma_path}")
 
