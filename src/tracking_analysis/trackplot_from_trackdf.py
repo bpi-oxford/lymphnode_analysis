2ee1cbb88576b3ea23d3ba2d45322f54169b219b
Ed Wheeler <edward.wheeler@kellogg.ox.ac.uk>
2025-09-23 11:29:58 +0100

    testing

--- src/tracking_analysis/trackplot_from_trackdf.py
+++ src/tracking_analysis/trackplot_from_trackdf.py
@@ -37,13 +37,41 @@ def plot_tracks_from_zero_3d(track_df):
     ax.legend()
     plt.show()
 
+def plot_tracks_from_zero_2d(track_df):
+    """
+    Plots tracks from a pandas DataFrame in 2D, aligning all tracks to start at (0, 0).
+    
+    Parameters:
+        track_df (pd.DataFrame): DataFrame containing track data with columns 'track_id', 'x', and 'y'.
+    """
+    plt.figure(figsize=(10, 8))
+    track_ids = track_df['track_id'].unique()
+    
+    for i in range(len(track_ids)):
+        track_id = track_ids[i]
+        track_data = track_df[track_df['track_id'] == track_id]
+
+        # Shift each track to start at (0, 0)
+        x_shifted = track_data['x'] - track_data['x'].iloc[0]
+        y_shifted = track_data['y'] - track_data['y'].iloc[0]
+
+        #find track velocity
+        avg_vel = np.round(avg_velocity_from_track(track_data) * 60, 1)
+        print(f"Track ID: {track_id}, Average Velocity: {avg_vel:.2f} um/min")
+        # Plot the shifted track with bolder lines
+        plt.plot(x_shifted, y_shifted, alpha=0.7, linewidth=3)
+    
+    plt.axis('off')
+    plt.show()
+
+
 # Example usage
 if __name__ == "__main__":
     # Example DataFrame
-    trackdf_path = r"/home/edwheeler/Documents/tissue_analysis_project/tracking_benchmarking/outputs/tracks_crop1_15.csv"
+    trackdf_path = r"/Users/edwheeler/Downloads/tracks_crop1_15.csv"
     track_df = pd.read_csv(trackdf_path)
     track_df = track_df[['track_id', 'x', 'y', 'z' , 't']]
     track_df = track_df[track_df['track_id'] <10]
     track_df = track_df[track_df['track_id'] > 0]
     
-    plot_tracks_from_zero_3d(track_df)
+    plot_tracks_from_zero_2d(track_df)
